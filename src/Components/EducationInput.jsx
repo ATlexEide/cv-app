8f18202715b7633592e30c6e8282b711d61036a3
Alex <atlexeide@gmail.com>
2025-02-03 11:59:12 +0100

    Add Education input component

--- src/Components/EducationInput.jsx
+++ src/Components/EducationInput.jsx
@@ -1,2 +1,29 @@
-function EducationInput() {}
+import { useState } from "react";
+
+function EducationInput({ label, name, callback }) {
+  const [schoolInfo, setSchoolInfo] = useState({
+    schoolName: "",
+    to: "",
+    from: "",
+  });
+  return (
+    <>
+      <label htmlFor={name}>{label}: </label>
+      <input
+        id={name}
+        type="text"
+        onChange={(e) => {
+          setSchoolInfo({ ...schoolInfo, schoolName: e.target.value });
+        }}
+      />
+      <button
+        onClick={() => {
+          callback(schoolInfo);
+        }}
+      >
+        Add
+      </button>
+    </>
+  );
+}
 export default EducationInput;
