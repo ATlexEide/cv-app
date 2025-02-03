eb02a30d2bca333f9a3d6ee39a6531c34f72ae4f
Alex <atlexeide@gmail.com>
2025-02-03 11:18:37 +0100

    Add test data

--- src/Components/EcucationalExpForm.jsx
+++ src/Components/EcucationalExpForm.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
 function EducationalExpForm() {
+  const testData = [
+    {
+      school: "arna",
+      from: "some time",
+      to: "some other time",
+    },
+    {
+      school: "damsgård",
+      from: "some time",
+      to: "some other time",
+    },
+    {
+      school: "holen",
+      from: "some time",
+      to: "some other time",
+    },
+  ];
+  const [education, setEducation] = useState(testData);
   return (
     <>
       <h2>Edu</h2>
+      <ul>
+        {education.map((item, index) => (
+          <li key={index}>{item.school}</li>
+        ))}
+      </ul>
     </>
   );
 }
