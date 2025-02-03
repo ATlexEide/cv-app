f9a036e50e03b6906788702a7edfa9354475a211
Alex <atlexeide@gmail.com>
2025-02-03 10:52:06 +0100

    Refac

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
-import "./App.css";
+import "./styles/App.css";
 import { useState } from "react";
-import Input from "./Components/Input";
+import DetailsForm from "./Components/DetailsForm";
 function App() {
-  const [person, setPerson] = useState({
-    firstName: "Navn",
-    lastName: "Navnesen",
-    email: "",
-    phone: "",
-  });
-  const handleFirstNameChange = (e) => {
-    setPerson({ ...person, firstName: e.target.value });
-    console.log(person);
-  };
-  const handleLastNameChange = (e) => {
-    setPerson({ ...person, lastName: e.target.value });
-    console.log(person);
-  };
   return (
     <>
-      <h1>{`${person.firstName} ${person.lastName}`}</h1>
-      <Input
-        label="First Name"
-        name="first-name"
-        value={person.firstName}
-        callback={handleFirstNameChange}
-      />
-      <Input
-        label="Last Name"
-        name="last-name"
-        value={person.lastName}
-        callback={handleLastNameChange}
-      />
+      <DetailsForm />;
     </>
   );
 }
