0250bb6d4ed8f7e7f99b4f30c2d1a08378c27057
Alex <atlexeide@gmail.com>
2025-02-03 10:42:46 +0100

    Update logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,33 @@ import "./App.css";
 import { useState } from "react";
 import Input from "./Components/Input";
 function App() {
-  const [person, setPerson] = useState("");
-  const handleOnChange = (e) => {
-    console.log(e.target.value);
+  const [person, setPerson] = useState({
+    firstName: "Navn",
+    lastName: "Navnesen",
+  });
+  const handleFirstNameChange = (e) => {
+    setPerson({ ...person, firstName: e.target.value });
+    console.log(person);
+  };
+  const handleLastNameChange = (e) => {
+    setPerson({ ...person, lastName: e.target.value });
+    console.log(person);
   };
   return (
     <>
-      <Input label="Test" name="test" callback={handleOnChange} />
+      <h1>{`${person.firstName} ${person.lastName}`}</h1>
+      <Input
+        label="First Name"
+        name="first-name"
+        value={person.firstName}
+        callback={handleFirstNameChange}
+      />
+      <Input
+        label="Last Name"
+        name="last-name"
+        value={person.lastName}
+        callback={handleLastNameChange}
+      />
     </>
   );
 }
