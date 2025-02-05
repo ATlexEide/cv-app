057778a39034ec360877571e4ae725b446f6c5bb
Alex <atlexeide@gmail.com>
2025-02-05 10:28:04 +0100

    Add preview call

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import "./styles/App.css";
 import DetailsForm from "./Components/DetailsForm";
 import EducationalExpForm from "./Components/EcucationalExpForm";
 import PracticalExpForm from "./Components/PracticalExpForm";
+import PreviewFile from "./Components/PreviewFile";
 function App() {
   return (
     <>
-      <DetailsForm />
-      <EducationalExpForm />
-      <PracticalExpForm />
+      <section id="inputs">
+        <DetailsForm />
+        <EducationalExpForm />
+        <PracticalExpForm />
+      </section>
+      <section id="preview">
+        <PreviewFile />
+      </section>
     </>
   );
 }
