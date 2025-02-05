aeab644cb0f8f567fc341a9e4022a8214890e476
Alex <atlexeide@gmail.com>
2025-02-05 12:52:33 +0100

    Add card component for education

---
+++ src/Components/EducationCard.jsx
@@ -0,0 +1,13 @@
+function EducationCard() {
+  return (
+    <>
+      <h3>Education</h3>
+      <div>
+        <h4>Some school</h4>
+        <div>Some degree</div>
+        <div>From some time to some other time</div>
+      </div>
+    </>
+  );
+}
+export default EducationCard;
