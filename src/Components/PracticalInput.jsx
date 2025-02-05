4d86ab77d9599032a9711b6651cf06c34453de15
Alex <atlexeide@gmail.com>
2025-02-05 10:14:14 +0100

    .

--- src/Components/PracticalInput.jsx
+++ src/Components/PracticalInput.jsx
@@ -1,4 +1,7 @@
+import Input from "./Input";
+
 function PracticalInput() {
-  return <h1>YIPPIE</h1>;
+  return <Input label="Company" name="company" />;
 }
 export default PracticalInput;
+// (company name, position title, main responsibilities of your jobs, date from and until when you worked for that company)
