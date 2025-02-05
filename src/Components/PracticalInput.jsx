677dfcdf9cc6482cb889cd9737f6d426e0f88542
Alex <atlexeide@gmail.com>
2025-02-05 10:19:22 +0100

    Update component return

--- src/Components/PracticalInput.jsx
+++ src/Components/PracticalInput.jsx
@@ -1,7 +1,13 @@
 import Input from "./Input";
 
 function PracticalInput() {
-  return <Input label="Company" name="company" />;
+  return (
+    <>
+      <Input label="Company" name="company" />
+      <Input label="Position" name="position" />
+      <Input type="textarea" label="Description" name="description" />
+    </>
+  );
 }
 export default PracticalInput;
 // (company name, position title, main responsibilities of your jobs, date from and until when you worked for that company)
