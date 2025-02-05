6803ad7872334fab474d7e6824e2a0b624e0bdd6
Alex <atlexeide@gmail.com>
2025-02-05 11:09:42 +0100

    Refactor contact info section

--- src/Components/PreviewFile.jsx
+++ src/Components/PreviewFile.jsx
@@ -3,7 +3,10 @@ function PreviewFile({ person }) {
   return (
     <article id="file">
       <h2>{`${person.firstName} ${person.lastName}`}</h2>
-      <h3>{`${person.email} ${person.phone}`}</h3>
+      <div id="contact-info">
+        <span id="email">{`Email: ${person.email}`}</span>
+        <span id="phonenumber">{`Tlf: ${person.phone}`}</span>
+      </div>
       <div className="border"></div>
     </article>
   );
