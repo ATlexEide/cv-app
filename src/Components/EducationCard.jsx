dd7d5d2d872e03b3cc5c0936652baa1c2b7c55eb
Alex <atlexeide@gmail.com>
2025-02-05 13:32:27 +0100

    Add description tags

--- src/Components/EducationCard.jsx
+++ src/Components/EducationCard.jsx
@@ -1,14 +1,18 @@
 import "../styles/PreviewFile.css";
-function EducationCard({ school, degree, timeFrom, timeTo }) {
+function EducationCard({ school, degree, timeFrom, timeTo, desc }) {
   return (
     <article>
       <div id="heading">
         <span id="date">{`${timeFrom} -> ${timeTo}`}</span>
         <h3 id="school">{school}</h3>
       </div>
-
       <h4 id="degree">{degree}</h4>
-      <p></p>
+      {desc && (
+        <div>
+          <h5>Extra info:</h5>
+          <p id="desc">{desc}</p>
+        </div>
+      )}
     </article>
   );
 }
