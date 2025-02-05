9d48596374eccd3fb54762f73f55ddca5aee6cfd
Alex <atlexeide@gmail.com>
2025-02-05 12:52:51 +0100

    Add card component for practical experience

---
+++ src/Components/PracticalCard.jsx
@@ -0,0 +1,13 @@
+function PracticalCard() {
+  return (
+    <>
+      <h3>Practical</h3>
+      <div>
+        <h4>Some work</h4>
+        <div>Some title</div>
+        <div>From some time to some other time</div>
+      </div>
+    </>
+  );
+}
+export default PracticalCard;
