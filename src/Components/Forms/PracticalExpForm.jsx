13dd4dab96019e234373ea67a7818f01d2e5bdb2
Alexander Eide <atlexeide@gmail.com>
2025-02-14 00:03:51 +0100

    Update form return

--- src/Components/Forms/PracticalExpForm.jsx
+++ src/Components/Forms/PracticalExpForm.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import PracticalInput from "../Inputs/PracticalInput";
 
-function PracticalExpForm({ addCard }) {
+function PracticalExpForm({ practical, addExperience }) {
+  const [practicalInfo, setPracticalInfo] = useState({
+    id: practical.length,
+    company: "",
+    title: "",
+    timeFrom: "",
+    timeTo: "",
+    desc: "",
+  });
   return (
     <>
-      <h2>Add practical experience</h2>
-      <PracticalInput />
-      <button onClick={addCard}>Add experience</button>
+      <h2>Practical experience</h2>
+      <PracticalInput
+        practicalInfo={practicalInfo}
+        setPracticalInfo={setPracticalInfo}
+      />
+      <button
+        onClick={() => {
+          console.log("practicalInfo", practicalInfo);
+          addExperience(practicalInfo, "practical");
+          setPracticalInfo({ ...practicalInfo, id: practicalInfo.id + 1 });
+        }}
+      >
+        Add experience
+      </button>
     </>
   );
 }
