f9b48e56d2600151f40283886b07f04f0e2c7c31
Alexander Eide <atlexeide@gmail.com>
2025-02-13 21:45:38 +0100

    Refactor data logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,67 @@ import EducationalExpForm from "./Components/Forms/EcucationalExpForm";
 import PracticalExpForm from "./Components/Forms/PracticalExpForm";
 import PreviewFile from "./Components/PreviewFile";
 function App() {
+  // TODO: Remove testdata when done
+  const testData = [
+    {
+      id: 0,
+      school: "Holen",
+      degree: "Comp Science",
+      timeFrom: "2001-01-01",
+      timeTo: "2002-02-02",
+      desc: "asdaqdasdasd",
+    },
+    {
+      id: 1,
+      school: "Damsgård",
+      degree: "Science Science",
+      timeFrom: "2001-01-01",
+      timeTo: "2002-02-02",
+      desc: "asdaqdasdasd",
+    },
+    {
+      id: 2,
+      school: "UwU School",
+      degree: "Furry Science",
+      timeFrom: "2001-01-01",
+      timeTo: "2002-02-02",
+      desc: "asdaqdasdasd",
+    },
+  ];
+  // TODO END
   const [person, setPerson] = useState({
     firstName: "Navn",
     lastName: "Navnesen",
     email: "mail@mail.no",
     phone: "12345678",
   });
+  const [education, setEducation] = useState(testData);
   const handleDetailChange = (e) => {
     setPerson({ ...person, [e.target.name]: e.target.value });
     console.log(person);
   };
-  const addCard = () => {
-    alert("yipp");
+  // TODO: Remove hardcoded value
+  const addExperience = (edu) => {
+    console.log(edu);
+    setEducation([...education, edu]);
   };
   return (
     <>
       <section id="inputs">
         <DetailsForm person={person} callback={handleDetailChange} />
-        <EducationalExpForm addCard={addCard} />
-        <PracticalExpForm addCard={addCard} />
+        <EducationalExpForm
+          education={education}
+          setEducation={setEducation}
+          addExperience={addExperience}
+        />
+        <PracticalExpForm addExperience={addExperience} />
       </section>
       <section id="preview">
-        <PreviewFile person={person} />
+        <PreviewFile
+          education={education}
+          setEducation={setEducation}
+          person={person}
+        />
       </section>
     </>
   );
