9e806a7bf50b54484971f82ee5a804f37e45a856
Alexander Eide <atlexeide@gmail.com>
2025-02-14 00:02:50 +0100

    Update practical card return

--- src/Components/Cards/PracticalCard.jsx
+++ src/Components/Cards/PracticalCard.jsx
@@ -1,12 +1,32 @@
-function PracticalCard() {
+function PracticalCard({ data, handleEdit, handleDelete }) {
   return (
-    <>
-      <div>
-        <h4>Some work</h4>
-        <div>Some title</div>
-        <div>From some time to some other time</div>
+    <article>
+      <div id="heading">
+        <span id="date-practical">{`${data.timeFrom} -> ${data.timeTo}`}</span>
+        <h3 id="company">{data.company}</h3>
       </div>
-    </>
+      <h4 id="title">{data.title}</h4>
+      {data.desc && (
+        <div>
+          <h5>Extra info:</h5>
+          <p id="desc-practical">{data.desc}</p>
+        </div>
+      )}
+      <button
+        onClick={() => {
+          handleEdit(data);
+        }}
+      >
+        Edit
+      </button>
+      <button
+        onClick={() => {
+          handleDelete(data.id);
+        }}
+      >
+        Remove
+      </button>
+    </article>
   );
 }
 export default PracticalCard;
