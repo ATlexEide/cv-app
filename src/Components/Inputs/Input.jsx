3a21cb1a1a76420984fdb5d26c5f1a2d8ca26271
Alexander Eide <atlexeide@gmail.com>
2025-02-16 01:10:01 +0100

    Update placeholder value

--- src/Components/Inputs/Input.jsx
+++ src/Components/Inputs/Input.jsx
@@ -18,7 +18,7 @@ function Input({
           type={type}
           value={value}
           onChange={onChange}
-          placeholder={placeholder && name}
+          placeholder={placeholder && label}
         />
       ) : (
         <input
