5d74dd7a95e80399383443de754f78cec1534cd1
Alex <atlexeide@gmail.com>
2025-02-18 06:53:53 +0100

    Clean up test data

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,19 @@
 import "./styles/App.css";
+import { educationData, practicalData } from "./testData";
 import { useState } from "react";
 import DetailsForm from "./Components/Forms/DetailsForm";
 import ExperienceForm from "./Components/Forms/ExperienceForm";
 import PreviewFile from "./Components/PreviewFile";
 import EditDialog from "./Components/EditDialog";
 function App() {
-  // TODO: Remove testdata when done
-  const testData = [
-    {
-      id: crypto.randomUUID(),
-      school: "Holen",
-      degree: "Comp Science",
-      timeFrom: "2001-01-01",
-      timeTo: "2002-02-02",
-      desc: "asdaqdasdasd",
-    },
-    {
-      id: crypto.randomUUID(),
-      school: "Damsgård",
-      degree: "Science Science",
-      timeFrom: "2001-01-01",
-      timeTo: "2002-02-02",
-      desc: "asdaqdasdasd",
-    },
-    {
-      id: crypto.randomUUID(),
-      school: "UwU School",
-      degree: "Furry Science",
-      timeFrom: "2001-01-01",
-      timeTo: "2002-02-02",
-      desc: "asdaqdasdasd",
-    },
-  ];
-  const testData2 = [
-    {
-      id: crypto.randomUUID(),
-      company: "Some workplace",
-      title: "Comp Science",
-      timeFrom: "2001-01-01",
-      timeTo: "2002-02-02",
-      desc: "asdaqdasdasd",
-    },
-    {
-      id: crypto.randomUUID(),
-      company: "Some other workplace",
-      title: "Science Science",
-      timeFrom: "2001-01-01",
-      timeTo: "2002-02-02",
-      desc: "asdaqdasdasd",
-    },
-  ];
-  // TODO END
   const [person, setPerson] = useState({
     firstName: "Navn",
     lastName: "Navnesen",
     email: "mail@mail.no",
     phone: "12345678",
   });
-  const [education, setEducation] = useState(testData);
-  const [practical, setPractical] = useState(testData2);
+  const [education, setEducation] = useState(educationData);
+  const [practical, setPractical] = useState(practicalData);
   const [isEditing, setIsEditing] = useState(false);
   const [currentCard, setCurrentCard] = useState({});
 
---
+++ src/testData.js
@@ -0,0 +1,44 @@
+export const educationData = [
+  {
+    id: crypto.randomUUID(),
+    school: "Holen",
+    degree: "Comp Science",
+    timeFrom: "2001-01-01",
+    timeTo: "2002-02-02",
+    desc: "asdaqdasdasd",
+  },
+  {
+    id: crypto.randomUUID(),
+    school: "Damsgård",
+    degree: "Science Science",
+    timeFrom: "2001-01-01",
+    timeTo: "2002-02-02",
+    desc: "asdaqdasdasd",
+  },
+  {
+    id: crypto.randomUUID(),
+    school: "UwU School",
+    degree: "Furry Science",
+    timeFrom: "2001-01-01",
+    timeTo: "2002-02-02",
+    desc: "asdaqdasdasd",
+  },
+];
+export const practicalData = [
+  {
+    id: crypto.randomUUID(),
+    company: "Some workplace",
+    title: "Comp Science",
+    timeFrom: "2001-01-01",
+    timeTo: "2002-02-02",
+    desc: "asdaqdasdasd",
+  },
+  {
+    id: crypto.randomUUID(),
+    company: "Some other workplace",
+    title: "Science Science",
+    timeFrom: "2001-01-01",
+    timeTo: "2002-02-02",
+    desc: "asdaqdasdasd",
+  },
+];
