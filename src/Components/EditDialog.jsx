6877819edfd6978e1fa1c160821742efba7fa36c
Alex <atlexeide@gmail.com>
2025-02-18 10:18:07 +0100

    Refactor input rendering

--- src/Components/EditDialog.jsx
+++ src/Components/EditDialog.jsx
@@ -64,95 +64,83 @@ function EditDialog({
   const handleCancel = () => {
     setIsEditing(false);
   };
+  const getValue = (id) => {
+    switch (id) {
+      case "school":
+        return school;
+      case "company":
+        return company;
+      case "degree":
+        return degree;
+      case "title":
+        return title;
+      case "desc":
+        return desc;
+      case "from":
+        return from;
+      case "to":
+        return to;
+
+      default:
+        break;
+    }
+  };
+  const updateInputValue = (input, value) => {
+    console.log(input);
+    console.log(value);
+    switch (input.id) {
+      case "school":
+        setSchool(value);
+        break;
+      case "company":
+        setCompany(value);
+        break;
+      case "degree":
+        setDegree(value);
+        break;
+      case "title":
+        setTitle(value);
+        break;
+      case "from":
+        setFrom(value);
+        break;
+      case "to":
+        setTo(value);
+        break;
+      case "desc":
+        setDesc(value);
+        break;
+
+      default:
+        break;
+    }
+    console.log(input);
+  };
+
   return (
     <form id="editDialog">
       <h2>Edit entry</h2>
 
-      {Object.keys(card).map(
+      {Object.entries(card).map(
         (input) =>
-          input !== "id" && (
+          input[0] !== "id" && (
             <Input
-              key={input}
+              key={input[0]}
               className="expInput"
-              label={Array.from(input)
-                .toSpliced(0, 1, input.at(0).toUpperCase())
+              label={Array.from(input[0])
+                .toSpliced(0, 1, input[0].at(0).toUpperCase())
                 .join("")}
-              type={input}
-              name={input.toLowerCase()}
-              placeholder={true}
-              onChange={(e) => {}}
+              type={input[0]}
+              name={input[0].toLowerCase()}
+              value={getValue(input[0])}
+              onChange={(e) => {
+                updateInputValue(e.target, e.target.value);
+                setNewCard({ ...newCard, [e.target.id]: e.target.value });
+              }}
             />
           )
       )}
-      {/* <Input
-        label={card.school ? "School" : "Company"}
-        name={card.school ? school : company}
-        type="text"
-        value={card.school ? school : company}
-        onChange={(e) => {
-          {
-            card.school && setSchool(e.target.value);
-          }
-          {
-            card.company && setCompany(e.target.value);
-          }
-          {
-            card.school && setNewCard({ ...newCard, school: e.target.value });
-          }
-          {
-            card.company && setNewCard({ ...newCard, company: e.target.value });
-          }
-        }}
-      />
-      <Input
-        label={card.school ? "Degree" : "Title"}
-        name={card.school ? degree : title}
-        value={card.school ? degree : title}
-        type="text"
-        onChange={(e) => {
-          {
-            card.school && setDegree(e.target.value);
-          }
-          {
-            card.company && setTitle(e.target.value);
-          }
-          {
-            card.school && setNewCard({ ...newCard, degree: e.target.value });
-          }
-          {
-            card.company && setNewCard({ ...newCard, title: e.target.value });
-          }
-        }}
-      />
-      <Input
-        label="Desc"
-        name={desc}
-        value={desc}
-        type="textarea"
-        onChange={(e) => {
-          setDesc(e.target.value);
-          setNewCard({ ...newCard, desc: e.target.value });
-        }}
-      />
-      <Input
-        label="From"
-        value={from}
-        type="date"
-        onChange={(e) => {
-          setFrom(e.target.value);
-          setNewCard({ ...newCard, timeFrom: e.target.value });
-        }}
-      />
-      <Input
-        label="To"
-        name={to}
-        value={to}
-        onChange={(e) => {
-          setTo(e.target.value);
-          setNewCard({ ...newCard, timeTo: e.target.value });
-        }}
-        type="date"
-      /> */}
+
       <button className="applyBtn" onClick={handleUpdate}>
         Apply changes
       </button>
