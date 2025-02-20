62307963022873490ad81b540ef4b004918bbe09
Alexander Eide <atlexeide@gmail.com>
2025-02-20 20:07:00 +0100

    Update card logic

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,22 +1,49 @@
-function Card({ skill = false, skillName, data, handleEdit, handleDelete }) {
+function Card({
+  workexp = false,
+  skill = false,
+  skillName,
+  data,
+  handleEdit,
+  handleDelete,
+}) {
   if (skill)
     return (
-      <section className="skill info">
+      <section className="skill info card">
         <h3>
           <i className={`fa-brands fa-${skillName.toLowerCase()}`}></i>
           {skillName}
         </h3>
       </section>
     );
-
+  if (data) {
+  }
   const type = data.school ? "education" : "practical";
   const place = data.school ? data.school : data.company;
   const designation = data.school ? data.degree : data.title;
+  if (workexp)
+    return (
+      <section className="workexp card" id={data.id}>
+        <h3>
+          <span className="timespan">{`${data.from.replace(/-/g, "/")} - ${
+            data.isCurrent ? "current" : data.to.replace(/-/g, "/")
+          }`}</span>{" "}
+          | {data.company}
+        </h3>
+        <h4>{data.title}</h4>
+        {data.desc && <p>{data.desc}</p>}
+        <CardButton
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+          data={data}
+          type={type}
+        />
+      </section>
+    );
 
   return (
-    <section className={`${type}-info`}>
+    <section className="school info card">
       <div className="year">
-        {data.from} - {data.to}
+        {data.from.replace(/-/g, "/")} - {data.to.replace(/-/g, "/")}
       </div>
       <h3>{place}</h3>
       <h4>{designation}</h4>
@@ -26,23 +53,12 @@ function Card({ skill = false, skillName, data, handleEdit, handleDelete }) {
           <p id="desc">{data.desc}</p>
         </>
       )}
-      <div className="cardbuttons">
-        <button
-          onClick={() => {
-            handleEdit(data);
-          }}
-        >
-          Edit
-        </button>
-
-        <button
-          onClick={() => {
-            handleDelete(data.id, type);
-          }}
-        >
-          Remove
-        </button>
-      </div>
+      <CardButton
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        data={data}
+        type={type}
+      />
     </section>
   );
 }
@@ -57,3 +73,25 @@ export default Card;
 //   </div>
 
 // </article>
+
+function CardButton(props) {
+  return (
+    <div className="cardbuttons">
+      <button
+        onClick={() => {
+          props.handleEdit(props.data);
+        }}
+      >
+        Edit
+      </button>
+
+      <button
+        onClick={() => {
+          props.handleDelete(props.data.id, props.type);
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
+}
