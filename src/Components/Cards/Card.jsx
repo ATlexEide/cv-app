11ab24b3a88d55ed8d4f0f485b2e139f4a1445eb
Alexander Eide <atlexeide@gmail.com>
2025-02-20 18:19:40 +0100

    Update card return / logic

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,47 +1,59 @@
-import "../../styles/PreviewFile.css";
+function Card({ skill = false, skillName, data, handleEdit, handleDelete }) {
+  if (skill)
+    return (
+      <section className="skill info">
+        <h3>
+          <i className={`fa-brands fa-${skillName.toLowerCase()}`}></i>
+          {skillName}
+        </h3>
+      </section>
+    );
 
-function Card({ data, handleEdit, handleDelete }) {
   const type = data.school ? "education" : "practical";
   const place = data.school ? data.school : data.company;
   const designation = data.school ? data.degree : data.title;
 
   return (
-    <article className="card">
-      <div id="heading">
-        <span id="date">{`${data.from} -> ${data.to}`}</span>
-
-        <h3 id={type}>{place}</h3>
-
-        <div className="cardbuttons">
-          <button
-            onClick={() => {
-              handleEdit(data);
-            }}
-          >
-            Edit
-          </button>
-
-          <button
-            onClick={() => {
-              handleDelete(data.id, type);
-            }}
-          >
-            Remove
-          </button>
-        </div>
+    <section className={`${type}-info`}>
+      <div className="year">
+        {data.from} - {data.to}
       </div>
-
-      <h4 id={designation}>{designation}</h4>
-
+      <h3>{place}</h3>
+      <h4>{designation}</h4>
       {data.desc && (
         <>
           <h5>Extra info:</h5>
-
           <p id="desc">{data.desc}</p>
         </>
       )}
-    </article>
+      <div className="cardbuttons">
+        <button
+          onClick={() => {
+            handleEdit(data);
+          }}
+        >
+          Edit
+        </button>
+
+        <button
+          onClick={() => {
+            handleDelete(data.id, type);
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    </section>
   );
 }
 
 export default Card;
+
+// <article className="card">
+//   <div id="heading">
+//     <span id="date">{`${data.from} -> ${data.to}`}</span>
+//     <h3 id={type}>{place}</h3>
+
+//   </div>
+
+// </article>
