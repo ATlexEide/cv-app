570429d6f4cda755eed518d5b08488c8fe375ba3
Alexander Eide <atlexeide@gmail.com>
2025-02-20 18:18:48 +0100

    Comment out data

--- src/testData.js
+++ src/testData.js
@@ -7,22 +7,22 @@ export const educationData = [
     to: "2002-02-02",
     desc: "asdaqdasdasd",
   },
-  {
-    id: crypto.randomUUID(),
-    school: "Damsgård",
-    degree: "Science Science",
-    from: "2001-01-01",
-    to: "2002-02-02",
-    desc: "asdaqdasdasd",
-  },
-  {
-    id: crypto.randomUUID(),
-    school: "UwU School",
-    degree: "Furry Science",
-    from: "2001-01-01",
-    to: "2002-02-02",
-    desc: "asdaqdasdasd",
-  },
+  // {
+  //   id: crypto.randomUUID(),
+  //   school: "Damsgård",
+  //   degree: "Science Science",
+  //   from: "2001-01-01",
+  //   to: "2002-02-02",
+  //   desc: "asdaqdasdasd",
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   school: "UwU School",
+  //   degree: "Furry Science",
+  //   from: "2001-01-01",
+  //   to: "2002-02-02",
+  //   desc: "asdaqdasdasd",
+  // },
 ];
 export const practicalData = [
   {
